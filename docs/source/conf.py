3afc573324c1ea0a30b887b80baf26bf121a189f
Jacob Vider <60020199+jacobvider@users.noreply.github.com>
2024-06-16 17:12:53 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'SuperLOTIS'
+copyright = '2024, Vider'
+author = 'Jacob Vider'
 
-release = '0.1'
-version = '0.1.0'
+# release = '0.1'
+# version = '0.1.0'
 
 # -- General configuration
 
